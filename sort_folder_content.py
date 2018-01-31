327898805c3ae75936c82f021e8037f898abafb3
sleepycerberus <slava.taras@gmail.com>
2018-01-31 10:18:15 +0200

    Added padding to the tabular output of the file names and sizes

--- sort_folder_content.py
+++ sort_folder_content.py
@@ -4,25 +4,42 @@ folder_path = "/Users/slavataras/Downloads/Software/Music/Arctic Monkeys - Favor
 my_folder = os.listdir(folder_path)
 
 
-def convert_bytes(num):  # this function will convert byte values to KB, MB, GB or TB using metric (SI) system
+# this function will convert byte values to KB, MB, GB or TB using metric (SI) system
+
+def convert_bytes(num):
 
     for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
         if num < 1000:
-            return "%3.1f %s" % (num, x)
+            return "%3.1f %s" % (num, x)  # displays up to 3 digits before decimal separator and one decimal digit after
         num /= 1000.0
 
 
-def longest_el(a_list):
+# this function will return the longest element in the given list for the eventual calculation of desired padding value
+# in "list_folder" function
+
+def longest_element(a_list):
     longest_element_length = 0
     for element in a_list:
         if len(element) > longest_element_length:
             longest_element_length = len(element)
-    # padding = " " * (longest_element_length + 5)
     return longest_element_length
 
 
-for item_name in my_folder:
-    item_full_path = folder_path + "/" + item_name
-    item_size = os.stat(item_full_path).st_size
-    print "%s %s" % (item_name, convert_bytes(item_size))
+# this function will print items in the given list (folder) and their respective sizes (in readable format) in the form
+# of a table, where a padding value between columns is calculated based on the longest item name in the given list
+
+def list_folder(a_folder):
+    padding = longest_element(a_folder) + 10
+    for item_name in a_folder:
+        item_full_path = os.path.join(folder_path, item_name)
+        item_size = os.stat(item_full_path).st_size
+        print item_name.ljust(padding),
+        print convert_bytes(item_size)
+
+
+list_folder(my_folder)
+
+
+
+
 
